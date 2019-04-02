4cfc5043db2b773b01b1eabd8f477063298c0c83
Katie Brown <hizzity@gmail.com>
2019-04-01 19:26:27 -0500

    Completed Web Caesar!

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-                           # is form = """ in correct spot
-form =                                                                       
+                           
+form =  """                                                                     
 <!DOCTYPE html>
 <html>
     <head>
@@ -26,31 +26,36 @@ form =
         </style>        
     </head>
     <body>
-       <form action="/" method="gett">
-                <label for="rot">Rotate by:</label>                                       
-                <input value = '0' type="text" name="rot"/>            
+       <form action="" method='post'>
+                <label for="rot">Rotate by:                                     
+                <input value = '0' id ="rot" type="text" name="rot"/> 
+                </label>             
                 <br>
-                <input id= type="textarea" name="text" size = "textarea"/>         <!-- how to use 'textarea' above -->
+                <textarea class = "textarea" name="text"/>  
+                {0}    
+                </textarea>
+                
                 <br>
                 <input type="submit" value = "Submit Query"/>
+        </form>
     </body>
 </html>
+"""
 
 
 
+
+@app.route("/", methods=['POST'])                           ## change to 'post' request
+def encrypt():                                   ## how to pull user input for rot and text from form
+    rotation = int(request.form["rot"])
+    text = request.form["text"]
+    encrypted_text = rotate_string(text, rotation)
+    return form.format(encrypted_text)                   ##<h1> ???                                   
+    
 
 @app.route("/", methods=['get'])                        ## how use form.format(...)
 def index():
-    return form.format(form)
+    return form.format("")
 
-@app.route("/", methods=['get'])                           ## change to 'post' request
-def encrypt(text, rot):                                   ## how to pull user input for rot and text from form
-    rotation = int(rot)
-    encrypted_text = rotate_string(text, rotation)
-    return """                                                          
-    <html>                                                  <! how use form.format(...)
-        <h1> encrypted_text </h1>
-    </html>
-    """
 
 app.run()
